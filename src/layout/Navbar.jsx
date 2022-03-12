2adc05e3503545ba22c12fb1952f85c3d4155268
gurjung <gurjung997@gmail.com>
2022-03-12 14:46:13 +0530

    feat: add navbar component

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Select,
+  MenuItem,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
   return (
-    <div>Navbar</div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography onClick={() => navigate("/")} className="Nav-title">
+              CryptoTracker
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
 
-export default Navbar
+export default Navbar;
