08fb39806ee382f62f16b32f04f970d10bd11418
gurjung <gurjung997@gmail.com>
2022-03-12 15:45:02 +0530

    chore: adds banner image and component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
+const Banner = () => {
+  return (
+    <div style={{ backgroundImage: "url(./banner2.jpg)" }}>
+      <Container className="Banner-content">
+        <div className="Banner-title">
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            Crypto Tracker
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
