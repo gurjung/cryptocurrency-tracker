ff3d3a64403a0f210be83e1d2dc3dda4b2e62663
gurjung <gurjung997@gmail.com>
2022-03-12 13:48:45 +0530

    chore: add navbar and homepage component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Homepage} from "./components";
+import { Navbar } from "./layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Hello World
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
